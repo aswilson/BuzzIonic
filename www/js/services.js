123bee43da7bd16b5861b1ddef7c22b1801e4504
Allie Wilson <aswilson@andrew.cmu.edu>
2015-09-11 23:46:31 -0400

    made a factory for queues

--- www/js/services.js
+++ www/js/services.js
@@ -53,44 +53,44 @@ services.factory('Queues', function() {
   // Might use a resource here that returns a JSON array
 
   // Some fake testing data
-  var chats = [{
+  var queues = [{
     id: 0,
     name: 'Ben Sparrow',
-    lastText: 'You on your way?',
+    message: 'You on your way?',
     face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
   }, {
     id: 1,
     name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
+    message: 'Hey, it\'s me',
     face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
   }, {
     id: 2,
     name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
+    message: 'I should buy a boat',
     face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
   }, {
     id: 3,
     name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
+    message: 'Look at my mukluks!',
     face: 'https://pbs.twimg.com/profile_images/598205061232103424/3j5HUXMY.png'
   }, {
     id: 4,
     name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
+    message: 'This is wicked good ice cream.',
     face: 'https://pbs.twimg.com/profile_images/578237281384841216/R3ae1n61.png'
   }];
 
   return {
     all: function() {
-      return chats;
+      return queues;
     },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
+    remove: function(queue) {
+      queues.splice(queues.indexOf(queue), 1);
     },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
+    get: function(queueId) {
+      for (var i = 0; i < queues.length; i++) {
+        if (queues[i].id === parseInt(queueId)) {
+          return queues[i];
         }
       }
       return null;
