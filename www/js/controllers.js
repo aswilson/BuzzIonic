e6d29b61e83a83d1935288f84cc276343cb29f7e
Allie Wilson <aswilson@andrew.cmu.edu>
2015-09-11 23:53:53 -0400

    changed chats tab content to link to new queues pages/detail and factory

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -21,6 +21,25 @@ angular.module('starter.controllers', [])
   $scope.chat = Chats.get($stateParams.chatId);
 })
 
+.controller('QueuesCtrl', function($scope, Queues) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  $scope.queues = Queues.all();
+  $scope.remove = function(queue) {
+    Queues.remove(queue);
+  };
+})
+
+.controller('QueueDetailCtrl', function($scope, $stateParams, Queues) {
+  $scope.queue = Queues.get($stateParams.queueId);
+})
+
 .controller('AccountCtrl', function($scope) {
   $scope.settings = {
     enableFriends: true
